4383fd979b66739e92c62c25b6bb1547f18d65fd
Youssef Beltagy <ybeltagy@amazon.com>
2026-01-02 06:59:43 +0000

    finished day 9 (approach 4.2)

--- src/day9.cpp
+++ src/day9.cpp
@@ -7,6 +7,7 @@
 #include <limits>
 #include <list>
 #include <utility>
+#include <algorithm>
 
 using namespace std;
 
@@ -71,8 +72,11 @@ using namespace std;
  * Approach 4:
  * Assessing all the rects is still too slow.
  * I can either:
- *   1. Do approach 2 to optimize finding the right rect. --> aborting this.
+ *   1. Do approach 2 to optimize finding the right rect. --> aborting this. Still too slow if I use a normal matrix.
  *   2. Continue with the same approach of assessing rects. But use sparse matrices to vastly improve efficiency.
+ *      Done! It works and is fast! Perhaps I can optimize this more. BUT I DON'T CARE!
+ *      I DON'T CARE ANYMORE! I DON'T CARE AT ALL. YOU ARE JUST AN ERROR THAT I USED TO THROW!
+ *                          https://youtu.be/u47C-zIQNRE
  *
  *
  *
@@ -127,6 +131,38 @@ void d9p1()
     cout << "Part 1: " << max_area << endl;
 }
 
+// ---------
+// I probably should make this a library
+struct D9InnerRange
+{
+    uint64_t st;
+    uint64_t end;
+
+    D9InnerRange(uint64_t st, uint64_t end) : st(st), end(end) {}
+};
+
+bool operator==(const D9InnerRange &r1, const D9InnerRange &r2)
+{
+    return r1.st == r2.st && r1.end == r2.end;
+}
+
+bool operator<(const D9InnerRange &r1, const D9InnerRange &r2)
+{
+    if (r1.st != r2.st)
+        return r1.st < r2.st;
+
+    return r1.end < r2.end;
+}
+
+struct D9OuterRange
+{
+    uint64_t st;
+    uint64_t end;
+    deque<D9InnerRange> innerRanges;
+
+    D9OuterRange(uint64_t st, uint64_t end) : st(st), end(end) {}
+};
+
 struct D9Rect
 {
     uint64_t area;
@@ -150,125 +186,117 @@ struct D9Matrix
 {
 
     D9Coordinate min_coor, max_coor;
-    vector<vector<uint64_t>> matrix;
+    deque<D9OuterRange> matrix;
 
     D9Matrix(const D9Coordinate &min_coor, const D9Coordinate &max_coor, const vector<D9Coordinate> &coordinates) : min_coor(min_coor), max_coor(max_coor)
     {
+        matrix = deque<D9OuterRange>(max_coor.r - min_coor.r + 1, {0, 0});
 
-        matrix = vector<vector<uint64_t>>(max_coor.r - min_coor.r + 1, vector<uint64_t>(max_coor.c - min_coor.c + 1, 0));
+        // cout << coordinates.size() << endl; => 496
+        // Matrix is: 96428x96754 (row x col)
 
-        // cout << coordinates.size() << endl; // 496
-        // cout << matrix.size() << ", " << matrix.front().size() << endl; // 96428, 96754
-
-        // 500 * 100_000 * 100_000 = 5 trillion.
-        for (size_t i = 0; i < coordinates.size(); i++)
+        for (uint64_t i = 0; i < coordinates.size(); i++)
         {
             // Get the previous coordinate assuming the coordinates are in a circle
             D9Coordinate prev = coordinates[(i - 1 + coordinates.size()) % coordinates.size()];
+            // Anchor the coordinates with respect to min_coor instead of 0,0 so we deal with smaller numbers.
             prev = {prev.r - min_coor.r, prev.c - min_coor.c};
 
             D9Coordinate cur = coordinates[i];
             cur = {cur.r - min_coor.r, cur.c - min_coor.c};
 
-            size_t r = min(prev.r, cur.r), R = max(prev.r, cur.r);
-            size_t c = min(prev.c, cur.c), C = max(prev.c, cur.c);
-            if (r != R)
-            { // Same column different rows
-                for (; r <= R; r++)
-                {
-                    matrix[r][c] = 1;
-                }
-            }
-            else
-            { // Same row different columns
+            uint64_t c = min(prev.c, cur.c), C = max(prev.c, cur.c);
+            for (uint64_t r = min(prev.r, cur.r), R = max(prev.r, cur.r); r <= R; r++)
+            {
+                // Same row different columns
+                // insert inner range
+                // Don't squash the outer ranges now. It is easier to squash them later.
 
-                // Avoid spamming X when filling columns because Xs are used to later fill in the greens
-                matrix[r][c] = 1;
-                c++;
-                for (; c <= C; c++)
-                {
-                    matrix[r][c] = 2;
-                }
+                matrix[r].st = r;
+                matrix[r].end = r;
+                D9InnerRange cur_range = {c, C};
+                deque<D9InnerRange> &ranges = matrix[r].innerRanges;
+                auto it = lower_bound(ranges.begin(), ranges.end(), cur_range);
+                ranges.insert(it, cur_range);
             }
         }
 
-        // print();
-
-        cout << "Filling in all the greens" << endl;
-
-        for (size_t r = 0, R = matrix.size(); r < R; r++)
+        // Filling in all the greens
+        for (uint64_t r = 0, R = matrix.size(); r < R; r++)
         {
+            deque<D9InnerRange> &ranges = matrix[r].innerRanges;
             /*
              * Since this is a closed loop, if you trace a line from one side of the matrix to the other,
              * You will notice that "inside" parts of the line cross an odd number of edges.
              * "Outside" parts of the line cross an even number of edges.
              */
-            uint64_t prev = 0;
-            for (size_t c = 0, C = matrix.front().size(); c < C; c++)
+            for (size_t i = 0; i + 1 < ranges.size(); i++)
             {
+                D9InnerRange &cur_range = ranges[i];
+                D9InnerRange &next_range = ranges[i + 1];
 
-                if (prev == 0 && matrix[r][c] == 1)
-                { // Crossing an edge. start coloring in green.
-                    prev = 1;
-                }
-                else if (prev == 1 && matrix[r][c] == 0)
-                { // Color in green
-                    matrix[r][c] = 1;
-                }
-                else if (prev == 1 && matrix[r][c] == 1)
-                { // Crossing an edge.  Stop coloring in green.
-                    prev = 0;
-                } // Remaining case: prev == '.' && matrix[r][c] == '.' --> just don't color in green
+                // merge ranges
+                cur_range.end = next_range.end;
 
-                if (matrix[r][c] == 2)
-                {
-                    // Now undo the edge condition added before.
-                    matrix[r][c] = 1;
-                }
+                ranges.erase(ranges.begin() + i + 1); // this can be optimized if I care enough
             }
         }
 
-        // print();
-    }
-
-    uint64_t solve()
-    {
-        // Precompute from left to right
-        for (size_t r = 0, R = matrix.size(); r < R; r++)
+        // Merge overlapping innerRanges to simplify the matrix
+        for (uint64_t r = 0, R = matrix.size(); r < R; r++)
         {
-            for (size_t c = 1, C = matrix.front().size(); c < C; c++)
+            deque<D9InnerRange> &ranges = matrix[r].innerRanges;
+
+            for (size_t i = 0; i + 1 < ranges.size();)
             {
-                if (matrix[r][c] == 1)
+                D9InnerRange &cur_range = ranges[i];
+                D9InnerRange &next_range = ranges[i + 1];
+
+                if (cur_range.end + 1 >= next_range.st)
                 {
-                    matrix[r][c] += matrix[r][c - 1];
+                    cur_range.end = next_range.end;
+                    ranges.erase(ranges.begin() + i + 1); // this can be optimized if I care enough
+                }
+                else
+                {
+                    i++;
                 }
             }
         }
 
-        // This will consume too much memory and be too slow.
-        // vector<pair<uint64_t, uint64_t>> st;
+        // Merge overlapping outerRanges to simplify the matrix
+        for (size_t i = 0; i + 1 < matrix.size();)
+        {
+            if (matrix[i].innerRanges == matrix[i + 1].innerRanges)
+            {
+                matrix[i].end = matrix[i + 1].end;
+                matrix.erase(matrix.begin() + i + 1);
+            }
+            else
+            {
+                i++;
+            }
+        }
 
-        // for (size_t c = 0, C = matrix.front().size(); c < C; c++)
+        // Shrink to save memory
+        // matrix.shrink_to_fit();
+        // for (auto it = matrix.begin(); it != matrix.end(); it++)
         // {
-        //     for (size_t r = 0, R = matrix.size(); r < R; r++)
-        //     {
-        //         while(!st.empty() && st.back().second > matrix[r][c]) st.pop_back();
-
-        //         if (st.empty() || st.back().second < matrix[r][c]) st.push_back({r, matrix[r][c]});
-        //     }
+        //     it->innerRanges.shrink_to_fit();
         // }
-
-        return 0;
     }
 
     void print()
     {
-        for (const vector<uint64_t> &row : matrix)
+        for (const D9OuterRange &row : matrix)
         {
-            for (const uint64_t c : row)
+            cout << row.st << "-" << row.end << ": ";
+
+            for (const D9InnerRange &r : row.innerRanges)
             {
-                cout << c << " ";
+                cout << r.st << "->" << r.end << " ";
             }
+
             cout << endl;
         }
     }
@@ -278,12 +306,34 @@ struct D9Matrix
         D9Coordinate t = {rect.t.r - min_coor.r, rect.t.c - min_coor.c};
         D9Coordinate b = {rect.b.r - min_coor.r, rect.b.c - min_coor.c};
 
-        for (size_t r = t.r, R = b.r; r <= R; r++)
-            for (size_t c = min(t.c, b.c), C = max(t.c, b.c); c <= C; c++)
+        for (uint64_t r = t.r, R = b.r; r <= R;)
+        {
+            auto rit = lower_bound(
+                matrix.begin(),
+                matrix.end(),
+                r,
+                [](const D9OuterRange &range, const uint64_t row) -> bool
+                { return range.end < row; });
+
+            const deque<D9InnerRange> &innerRanges = rit->innerRanges;
+
+            for (uint64_t c = min(t.c, b.c), C = max(t.c, b.c); c <= C;)
             {
-                if (matrix[r][c] != 1)
+                auto cit = lower_bound(
+                    innerRanges.begin(),
+                    innerRanges.end(),
+                    c,
+                    [](const D9InnerRange &range, const uint64_t col) -> bool
+                    { return range.end < col; });
+
+                if (cit == innerRanges.end() || cit->st > c)
                     return false;
+
+                c = cit->end + 1;
             }
+
+            r = rit->end + 1;
+        }
 
         return true;
     }
@@ -309,31 +359,28 @@ void d9p2()
     }
 
     D9Matrix matrix(min_coor, max_coor, coordinates);
-    cout << "Part 2: " << matrix.solve() << endl;
 
-    // cout << "Building Rects" << endl;
-    // priority_queue<D9Rect, vector<D9Rect>> pq_rect;
+    priority_queue<D9Rect, vector<D9Rect>> pq_rect;
 
-    // for (const D9Coordinate &t : coordinates)
-    //     for (const D9Coordinate &b : coordinates)
-    //     {
-    //         // Avoid considering the same coordinates twice
-    //         if (t.r > b.r || (t.r == b.r && t.c > b.c))
-    //             continue;
+    for (const D9Coordinate &t : coordinates)
+        for (const D9Coordinate &b : coordinates)
+        {
+            // Avoid considering the same coordinates twice
+            if (t.r > b.r || (t.r == b.r && t.c > b.c))
+                continue;
 
-    //         pq_rect.push(D9Rect(t, b));
-    //     }
+            pq_rect.push(D9Rect(t, b));
+        }
 
-    // cout << "Finding an answer" << endl;
-    // D9Rect rect = pq_rect.top();
-    // while (!pq_rect.empty())
-    // {
-    //     if (matrix.is_valid_rect(rect))
-    //         break;
+    D9Rect rect = pq_rect.top();
+    while (!pq_rect.empty())
+    {
+        if (matrix.is_valid_rect(rect))
+            break;
 
-    //     pq_rect.pop();
-    //     rect = pq_rect.top();
-    // }
+        pq_rect.pop();
+        rect = pq_rect.top();
+    }
 
-    // cout << "Part 2: " << rect.area << endl;
+    cout << "Part 2: " << rect.area << endl;
 }
