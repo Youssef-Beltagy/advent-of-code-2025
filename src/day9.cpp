acf63ba28f110609c40c74084d4570cce0b3386a
Youssef Beltagy <ybeltagy@amazon.com>
2026-01-02 05:05:40 +0000

    day 9 part 2 -- abort approach 4.1

--- src/day9.cpp
+++ src/day9.cpp
@@ -68,7 +68,11 @@ using namespace std;
  * Doing it with the approrach of pencil crossing from left to right is probably good enough
  *
  *
- *
+ * Approach 4:
+ * Assessing all the rects is still too slow.
+ * I can either:
+ *   1. Do approach 2 to optimize finding the right rect. --> aborting this.
+ *   2. Continue with the same approach of assessing rects. But use sparse matrices to vastly improve efficiency.
  *
  *
  *
@@ -146,12 +150,12 @@ struct D9Matrix
 {
 
     D9Coordinate min_coor, max_coor;
-    vector<vector<char>> matrix;
+    vector<vector<uint64_t>> matrix;
 
     D9Matrix(const D9Coordinate &min_coor, const D9Coordinate &max_coor, const vector<D9Coordinate> &coordinates) : min_coor(min_coor), max_coor(max_coor)
     {
 
-        matrix = vector<vector<char>>(max_coor.r - min_coor.r + 1, vector<char>(max_coor.c - min_coor.c + 1, '.'));
+        matrix = vector<vector<uint64_t>>(max_coor.r - min_coor.r + 1, vector<uint64_t>(max_coor.c - min_coor.c + 1, 0));
 
         // cout << coordinates.size() << endl; // 496
         // cout << matrix.size() << ", " << matrix.front().size() << endl; // 96428, 96754
@@ -172,18 +176,18 @@ struct D9Matrix
             { // Same column different rows
                 for (; r <= R; r++)
                 {
-                    matrix[r][c] = 'X';
+                    matrix[r][c] = 1;
                 }
             }
             else
             { // Same row different columns
 
                 // Avoid spamming X when filling columns because Xs are used to later fill in the greens
-                matrix[r][c] = 'X';
+                matrix[r][c] = 1;
                 c++;
                 for (; c <= C; c++)
                 {
-                    matrix[r][c] = 'G';
+                    matrix[r][c] = 2;
                 }
             }
         }
@@ -194,33 +198,32 @@ struct D9Matrix
 
         for (size_t r = 0, R = matrix.size(); r < R; r++)
         {
-
             /*
              * Since this is a closed loop, if you trace a line from one side of the matrix to the other,
              * You will notice that "inside" parts of the line cross an odd number of edges.
              * "Outside" parts of the line cross an even number of edges.
              */
-            char prev = '.';
+            uint64_t prev = 0;
             for (size_t c = 0, C = matrix.front().size(); c < C; c++)
             {
 
-                if (prev == '.' && matrix[r][c] == 'X')
+                if (prev == 0 && matrix[r][c] == 1)
                 { // Crossing an edge. start coloring in green.
-                    prev = 'X';
+                    prev = 1;
                 }
-                else if (prev == 'X' && matrix[r][c] == '.')
+                else if (prev == 1 && matrix[r][c] == 0)
                 { // Color in green
-                    matrix[r][c] = 'X';
+                    matrix[r][c] = 1;
                 }
-                else if (prev == 'X' && matrix[r][c] == 'X')
+                else if (prev == 1 && matrix[r][c] == 1)
                 { // Crossing an edge.  Stop coloring in green.
-                    prev = '.';
+                    prev = 0;
                 } // Remaining case: prev == '.' && matrix[r][c] == '.' --> just don't color in green
 
-                if (matrix[r][c] == 'G')
+                if (matrix[r][c] == 2)
                 {
                     // Now undo the edge condition added before.
-                    matrix[r][c] = 'X';
+                    matrix[r][c] = 1;
                 }
             }
         }
@@ -228,13 +231,43 @@ struct D9Matrix
         // print();
     }
 
+    uint64_t solve()
+    {
+        // Precompute from left to right
+        for (size_t r = 0, R = matrix.size(); r < R; r++)
+        {
+            for (size_t c = 1, C = matrix.front().size(); c < C; c++)
+            {
+                if (matrix[r][c] == 1)
+                {
+                    matrix[r][c] += matrix[r][c - 1];
+                }
+            }
+        }
+
+        // This will consume too much memory and be too slow.
+        // vector<pair<uint64_t, uint64_t>> st;
+
+        // for (size_t c = 0, C = matrix.front().size(); c < C; c++)
+        // {
+        //     for (size_t r = 0, R = matrix.size(); r < R; r++)
+        //     {
+        //         while(!st.empty() && st.back().second > matrix[r][c]) st.pop_back();
+
+        //         if (st.empty() || st.back().second < matrix[r][c]) st.push_back({r, matrix[r][c]});
+        //     }
+        // }
+
+        return 0;
+    }
+
     void print()
     {
-        for (const vector<char> &row : matrix)
+        for (const vector<uint64_t> &row : matrix)
         {
-            for (const char c : row)
+            for (const uint64_t c : row)
             {
-                cout << c;
+                cout << c << " ";
             }
             cout << endl;
         }
@@ -248,7 +281,7 @@ struct D9Matrix
         for (size_t r = t.r, R = b.r; r <= R; r++)
             for (size_t c = min(t.c, b.c), C = max(t.c, b.c); c <= C; c++)
             {
-                if (matrix[r][c] != 'X')
+                if (matrix[r][c] != 1)
                     return false;
             }
 
@@ -276,30 +309,31 @@ void d9p2()
     }
 
     D9Matrix matrix(min_coor, max_coor, coordinates);
+    cout << "Part 2: " << matrix.solve() << endl;
 
-    cout << "Building Rects" << endl;
-    priority_queue<D9Rect, vector<D9Rect>> pq_rect;
+    // cout << "Building Rects" << endl;
+    // priority_queue<D9Rect, vector<D9Rect>> pq_rect;
 
-    for (const D9Coordinate &t : coordinates)
-        for (const D9Coordinate &b : coordinates)
-        {
-            // Avoid considering the same coordinates twice
-            if (t.r > b.r || (t.r == b.r && t.c > b.c))
-                continue;
+    // for (const D9Coordinate &t : coordinates)
+    //     for (const D9Coordinate &b : coordinates)
+    //     {
+    //         // Avoid considering the same coordinates twice
+    //         if (t.r > b.r || (t.r == b.r && t.c > b.c))
+    //             continue;
 
-            pq_rect.push(D9Rect(t, b));
-        }
+    //         pq_rect.push(D9Rect(t, b));
+    //     }
 
-    cout << "Finding an answer" << endl;
-    D9Rect rect = pq_rect.top();
-    while (!pq_rect.empty())
-    {
-        if (matrix.is_valid_rect(rect))
-            break;
+    // cout << "Finding an answer" << endl;
+    // D9Rect rect = pq_rect.top();
+    // while (!pq_rect.empty())
+    // {
+    //     if (matrix.is_valid_rect(rect))
+    //         break;
 
-        pq_rect.pop();
-        rect = pq_rect.top();
-    }
+    //     pq_rect.pop();
+    //     rect = pq_rect.top();
+    // }
 
-    cout << "Part 2: " << rect.area << endl;
+    // cout << "Part 2: " << rect.area << endl;
 }
